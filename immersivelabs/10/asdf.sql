cff2c59630b01f115de32f4e395c07420f5ce4ba
henryfbp <henryfbp@gmail.com>
2021-11-18 22:41:45 -0500

    work on imlab10

---
+++ immersivelabs/10/asdf.sql
@@ -0,0 +1,9 @@
+select * from employees
+
+select * from employees where start_date = '14/10/2015'
+
+select * from employees where ((pay >= 55000) and (pay <= 62500)) order by pay desc
+
+select * from employees where email like 'B.Nixon%'
+
+select *  from employees order by pay desc
